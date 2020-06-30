99fecde6163a42e183d410effa528c5fd0e63f50
Dmitry <dmitriy@Bono-Dmitry-Smirnov-Vladimirovich.local>
2020-06-30 15:18:57 +0300

    commit

--- src/js/modules/scene/check-collision.js
+++ src/js/modules/scene/check-collision.js
@@ -2,14 +2,14 @@ import APP from '../../app'
 import * as THREE from 'three'
 
 const checkCollision = () => {
-  var originPoint = APP.obj.position.clone()
+  const originPoint = APP.obj.position.clone()
 
-  for (var vertexIndex = 0; vertexIndex < APP.obj.geometry.vertices.length; vertexIndex++) {
-    var localVertex = APP.obj.geometry.vertices[vertexIndex].clone()
-    var globalVertex = localVertex.applyMatrix4(APP.obj.matrix)
-    var directionVector = globalVertex.sub(APP.obj.position)
-    var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize())
-    var collisionResults = ray.intersectObjects(APP.collidableMeshList)
+  for (let vertexIndex = 0; vertexIndex < APP.obj.geometry.vertices.length; vertexIndex++) {
+    const localVertex = APP.obj.geometry.vertices[vertexIndex].clone()
+    const globalVertex = localVertex.applyMatrix4(APP.obj.matrix)
+    const directionVector = globalVertex.sub(APP.obj.position)
+    const ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize())
+    const collisionResults = ray.intersectObjects(APP.collidableMeshList)
     if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
       console.log('hit')
     } else {
